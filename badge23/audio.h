34ae9cfe1e2b0f49086395ae928d50fbab473deb
moon2 <moon2protonmail@protonmail.com>
2023-04-23 22:22:50 +0200

    UNFLIPPED SPEAKER PHASE (also some cleanups)

--- badge23/audio.h
+++ badge23/audio.h
@@ -7,18 +7,8 @@
 #define BADGE_VOLUME_LIMIT 30000
 
 void audio_init();
-void play_bootsound();
-void synth_set_freq(int i, float freq);
-void synth_set_vol(int i, float vol);
-void synth_set_bend(int i, float bend);
-void synth_start(int i);
-void synth_stop(int i);
-void synth_fullstop(int i);
-float synth_get_env(int i);
 
 void set_global_vol_dB(int8_t vol_dB);
-void set_extra_synth(void * synth);
-void clear_extra_synth();
 uint16_t count_audio_sources();
 
 uint16_t add_audio_source(void * render_data, void * render_function);
