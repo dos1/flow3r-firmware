a4be0596900cba716cef138fd9df22acbf6c220e
moon2 <moon2protonmail@protonmail.com>
2023-04-23 21:35:15 +0200

    minor bugfix

--- python_payload/cap_touch_demo.py
+++ python_payload/cap_touch_demo.py
@@ -6,19 +6,16 @@ synths = []
 for i in range(5):
     synths += [tinysynth(440,1)]
 
-chords = [[0,3,7,10,12],[-2,2,5,8,10],[i-2,3,7,10,14],[-4,0,3,8,12],[-1,2,5,7,11]]
+chords = [[0,3,7,10,12],[-2,2,5,8,10],[-2,3,7,10,14],[-4,0,3,8,12],[-1,2,5,7,11]]
 
-def tune_guitar_to_chord(chord):
-    for j in range(5):
-        frq = 440 * (2**(chord[j]/12))
-        synths[j].freq(int(frq))
-
-tune_guitar_to_chord(chords[2])
+chord = chords[3]
 
 while True:
     for i in range(10):
         if(get_captouch(i)):
             if(i%2):
-                tune_guitar_to_chord(chords[int((i-1)/2)])
+                chord = chords[int((i-1)/2)]
             else:
-                synths[int(i/2)].start()
+                i = int(i/2)
+                synths[i].freq(440*(2**(chord[i]/12)))
+                synths[i].start()
