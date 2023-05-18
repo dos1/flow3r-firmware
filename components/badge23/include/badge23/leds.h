f60914c7d09a7bca8b603d7940c677e9bdd95f32
moon2 <moon2protonmail@protonmail.com>
2023-05-18 23:52:19 +0200

    added led support kinda

--- components/badge23/include/badge23/leds.h
+++ components/badge23/include/badge23/leds.h
@@ -2,7 +2,6 @@
 
 #include <stdint.h>
 void leds_init();
-void leds_animate(int leaf);
 void leds_set_single_rgb(uint8_t index, uint8_t red, uint8_t green, uint8_t blue);
 void leds_set_single_hsv(uint8_t index, float hue, float sat, float value);
 void leds_update();
