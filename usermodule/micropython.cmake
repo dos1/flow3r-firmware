e1515c6bb109578c0bf6218c7f43de404cdb3c01
iggy <iggy@muc.ccc.de>
2023-05-21 18:46:34 +0200

    enable uctx as a usermodule

--- usermodule/micropython.cmake
+++ usermodule/micropython.cmake
@@ -15,3 +15,8 @@ target_include_directories(usermod_badge23 INTERFACE
 )
 
 target_link_libraries(usermod INTERFACE usermod_badge23)
+
+
+
+#Also add uctx as a usermodule
+include(${CMAKE_CURRENT_LIST_DIR}/uctx/uctx/micropython.cmake)
