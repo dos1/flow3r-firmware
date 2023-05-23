4de82eeb61202df0260e2a95d174dd51cd1ad6c3
moon2 <moon2protonmail@protonmail.com>
2023-05-23 22:41:07 +0200

    added crude mp api for positional cap touch

--- components/badge23/include/badge23/captouch.h
+++ components/badge23/include/badge23/captouch.h
@@ -7,5 +7,6 @@ void captouch_print_debug_info(void);
 void gpio_event_handler(void * arg);
 void manual_captouch_readout(uint8_t top);
 void captouch_get_cross(int paddle, int * x, int * y);
+uint16_t captouch_get_petal_pad_raw(uint8_t petal, uint8_t pad, uint8_t amb);
 void captouch_force_calibration();
 uint16_t read_captouch();
