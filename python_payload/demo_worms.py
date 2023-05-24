760d732f1b0288470395272d81bcd17bc75e4f0b
iggy <iggy@muc.ccc.de>
2023-05-24 02:49:27 +0200

    demo-worms: now with interaction, spawn worms with captouch

--- python_payload/demo_worms.py
+++ python_payload/demo_worms.py
@@ -2,6 +2,7 @@ import hardware
 import random
 import time
 import math
+import event
 
 def xy_from_polar(r,deg):
 	#rad = deg/180*math.pi
@@ -40,17 +41,23 @@ ctx.text_baseline = ctx.MIDDLE
 ctx.rgb(*BLUE).rectangle(-WIDTH/2,-HEIGHT/2,WIDTH,HEIGHT).fill()
 
 #Write some text
-ctx.move_to(0,0).rgb(*WHITE).text("Hi :)")
+ctx.move_to(0,0).rgb(*WHITE).text("touch me :)")
 hardware.display_update()
 
 
 class Worm():
-	def __init__(self):
+	def __init__(self,direction=None):
 		self.color = randrgb()
-		self.direction = random.random()*math.pi*2
-		self.size = 10
+		
+		if direction:
+			self.direction = direction
+		else:
+			self.direction = random.random()*math.pi*2
+		
+			
+		self.size = 50
 		self.speed = self.size/5
-		(x,y) = xy_from_polar(40, self.direction+90)
+		(x,y) = xy_from_polar(100, self.direction)
 		self.x = x
 		self.y= y
 		#(self.dx,self.dy) = xy_from_polar(1,self.direction)
@@ -66,11 +73,17 @@ class Worm():
 		
 	def mutate(self):
 		self.color =  ([max(0,min(1,x+((random.random()-0.5)*0.3))) for x in self.color])
+		self.size = 20
 		
 	
 	def move(self):
 		dist = math.sqrt(self.x**2+self.y**2)
-		self.size = (120-dist)/3
+		target_size = (130-dist)/3
+		
+		if self.size>target_size: self.size-=1
+		
+		if self.size<target_size: self.size+=1
+		
 		self.speed = self.size/5
 		
 		self.direction += (random.random()-0.5)*math.pi/4
@@ -80,7 +93,7 @@ class Worm():
 		self.y+=dy
 		
 		
-		if dist>110-self.size/2 and dist>self._lastdist:
+		if dist>120-self.size/2 and dist>self._lastdist:
 			polar_position=math.atan2(self.y,self.x)
 			dx=dx*-abs(math.cos(polar_position))
 			dy=dy*-abs(math.sin(polar_position))
@@ -90,9 +103,20 @@ class Worm():
 		
 worms = []
 
-for i in range(23):
+for i in range(0):
 	worms.append(Worm())
 	
+
+def handle_input(data):
+	if data.get("type","")=="captouch" and data.get("to")==1:
+		print(data)
+		worms.append(Worm(data.get("index",0)*2*math.pi/10+math.pi ))
+		if len(worms)>10:
+			worms.pop(0)
+
+engine = event.Engine()
+engine.add_input(event.Event(name="control",action=handle_input, 
+	condition=lambda data: data.get("type","")=="captouch" and data.get("to")==1))
 
 while True:
 	for w in worms:
@@ -100,8 +124,9 @@ while True:
 		w.move()
 	
 	hardware.display_update()
+	engine._eventloop_single()
 	time.sleep(0.001)
-print ("done")
+	
 
 #Known problems:
 #ctx.rotate(math.pi) turns the display black until powercycled
