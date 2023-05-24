4953bde39183814fac70ebbdcf6bf4f9d2a36498
moon2 <moon2protonmail@protonmail.com>
2023-05-24 14:26:18 +0200

    moved deadspot to downwards pointing bottom petal base pad

--- python_payload/cap_touch_demo.py
+++ python_payload/cap_touch_demo.py
@@ -12,14 +12,14 @@ def run():
     time.sleep_ms(30)
     for i in range(0,10,2):
         size = (hardware.get_captouch(i) * 4) + 4
-        x = 60 + (utils.captouch_get_rad(i)/600)
+        x = 70 + (utils.captouch_get_rad(i)/1000)
         x += (utils.captouch_get_phi(i)/600)*1j
         rot =  cmath.exp(2j *math.pi * i / 10)
         x = x * rot
         utils.draw_rect(int(x.imag+120-(size/2)),int(x.real+120-(size/2)),size,size,0b1111100000011111)
     for i in range(1,10,2):
         size = (hardware.get_captouch(i) * 4) + 4
-        x = 60 + (utils.captouch_get_rad(i)/600)
+        x = 70 + (utils.captouch_get_rad(i)/1000)
         x += (utils.captouch_get_phi(i)/600)*1j
         rot =  cmath.exp(2j *math.pi * i / 10)
         x = x * rot
