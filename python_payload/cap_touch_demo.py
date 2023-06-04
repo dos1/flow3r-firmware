ad00890d12ef5105c05f3438e71cff08e2c5e35c
Serge Bazanski <q3k@q3k.org>
2023-06-05 00:44:44 +0200

    python: fully migrate over to ctx
    This removes some leftover Python code that still used the old display_*
    calls for drawing instead of ctx.
    
    We are now left only with display_update, which we will replace by the
    new graphics stack soon.

--- python_payload/cap_touch_demo.py
+++ python_payload/cap_touch_demo.py
@@ -4,23 +4,25 @@ import cmath
 import math
 import time
 
+ctx = hardware.get_ctx()
+
 def init():
     pass
 
 def run():
-    hardware.display_fill(0)
+    ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
     time.sleep_ms(30)
     for i in range(10):
         size = (hardware.get_captouch(i) * 4) + 4
         size += int(max(0, sum([hardware.captouch_get_petal_pad(i, x) for x in range(0, 3+1)]) / 8000))
         x = 70 + (hardware.captouch_get_petal_rad(i)/1000)
         x += (hardware.captouch_get_petal_phi(i)/600)*1j
-        rot =  cmath.exp(2j *math.pi * i / 10)
+        rot = cmath.exp(2j * math.pi * i / 10)
         x = x * rot
-        col = 0b1111100000011111
+        col = (1.0, 0.0, 1.0)
         if i%2:
-            col = 0b0000011111111111
-        utils.draw_rect(int(x.imag+120-(size/2)),int(x.real+120-(size/2)),size,size,col)
+            col = (0.0, 1.0, 1.0)
+        ctx.rgb(*col).rectangle(-int(x.imag-(size/2)), -int(x.real-(size/2)), size, size).fill()
     hardware.display_update()
 
 def foreground():
