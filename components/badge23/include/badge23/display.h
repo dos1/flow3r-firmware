da6791e0e1743058d7429ad0b565c5a1bb1bce73
Serge Bazanski <q3k@q3k.org>
2023-06-05 01:35:30 +0200

    badge23: work around ctx initialization race condition
    Sometimes mpy code requests ctx too early, before the display (and thus
    ctx) are initialized.
    
    This is a quick-and-dirty workaround as we're planning on getting rid of
    this code path anyway.

--- components/badge23/include/badge23/display.h
+++ components/badge23/include/badge23/display.h
@@ -12,4 +12,4 @@ void display_draw_pixel(uint8_t x, uint8_t y, uint16_t col);
 uint16_t display_get_pixel(uint8_t x, uint8_t y);
 void display_fill(uint16_t col);
 
-extern Ctx *the_ctx;
+extern volatile Ctx *the_ctx;
