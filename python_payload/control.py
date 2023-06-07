179d0f24d6da794e71f46f91ee66ea6a7c0041f0
iggy <iggy@muc.ccc.de>
2023-06-07 02:36:39 +0200

    py: menu_tinysynth and menu_settings use the new audio api; controls; icons and general ui styling

--- python_payload/control.py
+++ python_payload/control.py
@@ -1,25 +1,42 @@
 import ui
 
 class Control():
-    def __init__(self,name, default=0, on_set=None):
+    def __init__(self,name, default=0, on_set=None, on_get=None, on_mod=None):
         #TODO inheritance from Control()
         self.name=name
         self.on_set = on_set
-        self._value = default
+        self.on_get = on_get
+        self.on_mod = on_mod
+
+        if not self.on_get:
+            self._value = default
         self.ui = ui.IconValue(label=self.name,size=60, value=self.get_value())
+        self.ui.get_value = self.get_normal_value
 
     def draw(self):
+        self.ui.value = self.get_value()
+        print("draw: value", self.get_value())
         self.ui.draw()
 
-    def get_value(self):
+    def get_normal_value(self):
+        v = self.get_value()
+        n = min(1,max(0,v))
+        return n
+
+    def get_value(self,update=True):
+        if update and self.on_get:
+            self._value = self.on_get()
         return self._value
 
     def set_value(self, value, do_trigger=True):
         self._value = value
-        self.ui.value = value
         if do_trigger:
             if self.on_set:
                 self.on_set(value)
+    
+    def mod_value(self,delta):
+        self._value = self.on_mod(delta)
+
     
     def enter(self):
         pass
@@ -33,6 +50,10 @@ class Control():
 class ControlSwitch(Control):
     def enter(self):
         self.set_value(not self.get_value())
+        self.draw()
+
+    def scroll(self,delta):
+        self.enter()
 
 class ControlFloat(Control):
     def __init__(self, min=0.0,max=1.0,step=0.1, *args, **kwargs):
@@ -41,15 +62,28 @@ class ControlFloat(Control):
         self.step=step
         super().__init__(*args, **kwargs)
 
+    def get_normal_value(self):
+        v = self.get_value()
+        n = (v-self.min)/(self.max-self.min)
+        #print("normal:",v,n)
+        return n
+
 class ControlKnob(ControlFloat):
     def enter(self):
         #repeat action with current value
         self.set_value(self.get_value())
+        self.draw()
 
     def scroll(self,delta):
-        v = self.get_value()
-        v_new = max(self.min,min(self.max,v+delta*self.step))
-        self.set_value(v_new)
+        if self.on_mod:
+            #print("control: on mod")
+            self.on_mod(delta)
+
+        elif self.on_set:
+            v = self.get_value()
+            v_new = max(self.min,min(self.max,v+delta*self.step))
+            self.set_value(v_new)
+        self.draw()
 
 class ControlSlide(ControlFloat):
     def __init__(self, do_reset=True, *args, **kwargs):
@@ -67,4 +101,4 @@ class ControlSlide(ControlFloat):
         if z<0: #Release
             if self.do_reset:
                 self.set_value(self._saved_value)
-        
+        self.draw()
--- python_payload/demo_menu.py
+++ python_payload/demo_menu.py
@@ -5,7 +5,7 @@ import control
 import audio
 import application
 
-#import demo_worms,demo_sparabo,cap_touch_demo, melodic_demo, harmonic_demo
+import demo_worms,demo_sparabo,cap_touch_demo, melodic_demo, harmonic_demo
 import menu_settings,menu_tinysynth
 
 import time
@@ -19,11 +19,11 @@ menu_apps = menu.Menu("apps")
 menu_music = menu.Menu("music")
 
 
-#for app_module in [demo_sparabo,melodic_demo,harmonic_demo]:
-#    menu_music.add(menu.MenuItemApp(app_module.app))
+for app_module in [demo_sparabo,melodic_demo,harmonic_demo]:
+    menu_music.add(menu.MenuItemApp(app_module.app))
 
-#for app_module in [demo_worms,cap_touch_demo,]:
-#    menu_apps.add(menu.MenuItemApp(app_module.app))
+for app_module in [demo_worms,cap_touch_demo,]:
+    menu_apps.add(menu.MenuItemApp(app_module.app))
 
 testmenu = menu.Menu("test")
 
@@ -40,11 +40,11 @@ testmenu.add(item_add)
 
 
 menu_badge.add(menu.MenuItemSubmenu(testmenu))
-menu_badge.add(menu.MenuItemSubmenu(menu_settings.get_menu()))
 
 menu_main.add(menu.MenuItemSubmenu(menu_badge))
 menu_main.add(menu.MenuItemSubmenu(menu_apps))
 menu_main.add(menu.MenuItemSubmenu(menu_music))
+menu_main.add(menu.MenuItemSubmenu(menu_settings.get_menu()))
 
 
 menu_music.add(menu.MenuItemSubmenu(menu_tinysynth.get_menu()))
