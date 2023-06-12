aa25b0b5b37869aa201cb524814ed565a3fdd9dd
Serge Bazanski <q3k@q3k.org>
2023-06-12 14:07:42 +0200

    sim: implement new leds api

--- sim/fakes/hardware.py
+++ sim/fakes/hardware.py
@@ -343,39 +343,10 @@ def display_update(subctx):
 def display_pipe_full():
     return False
 
-def set_led_rgb(ix, r, g, b):
-    ix = ((39-ix) + 1 + 32)%40;
-
-    r = r << 3
-    g = g << 2
-    b = b << 3
-    if r > 255:
-        r = 255
-    if g > 255:
-        g = 255
-    if b > 255:
-        b = 255
-    _sim.set_led_rgb(ix, r, g, b)
-
-
-def set_led_hsv(ix, h, s, v):
-    color = pygame.Color(0)
-    h /= 255.0
-    color.hsva = (h, s, v, 1.0)
-    r, g, b = color.r, color.g, color.b
-    r *= 255
-    g *= 255
-    b *= 255
-    _sim.set_led_rgb(ix, r, g, b)
-
-
-def update_leds():
-    _sim.leds_update()
-    _sim.render_gui_lazy()
-
 
 def set_global_volume_dB(a):
     pass
+
 
 def get_button_state(left):
     _sim.process_events()
---
+++ sim/fakes/leds.py
@@ -0,0 +1,34 @@
+from hardware import _sim
+
+import pygame
+
+
+def set_rgb(ix, r, g, b):
+    ix = ((39-ix) + 1 + 32)%40;
+
+    r = r << 3
+    g = g << 2
+    b = b << 3
+    if r > 255:
+        r = 255
+    if g > 255:
+        g = 255
+    if b > 255:
+        b = 255
+    _sim.set_led_rgb(ix, r, g, b)
+
+
+def set_hsv(ix, h, s, v):
+    color = pygame.Color(0)
+    h /= 255.0
+    color.hsva = (h, s, v, 1.0)
+    r, g, b = color.r, color.g, color.b
+    r *= 255
+    g *= 255
+    b *= 255
+    _sim.set_led_rgb(ix, r, g, b)
+
+
+def update():
+    _sim.leds_update()
+    _sim.render_gui_lazy()
