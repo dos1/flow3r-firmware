9075ea717ee729473cb59f46c3271eec8bd8e7ad
Serge Bazanski <q3k@q3k.org>
2023-06-25 22:08:20 +0200

    components/badge23: remove spurious i2c init

--- components/badge23/espan.c
+++ components/badge23/espan.c
@@ -22,41 +22,6 @@
 
 static const char *TAG = "espan";
 
-#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
-#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-
-#if defined(CONFIG_BADGE23_HW_GEN_P3) || defined(CONFIG_BADGE23_HW_GEN_P4) || defined(CONFIG_BADGE23_HW_GEN_P6)
-#define CONFIG_I2C_MASTER_SDA 2
-#define CONFIG_I2C_MASTER_SCL 1
-
-#elif defined(CONFIG_BADGE23_HW_GEN_P1)
-#define CONFIG_I2C_MASTER_SDA 10
-#define CONFIG_I2C_MASTER_SCL 9
-
-#else
-#error "i2c not implemented for this badge generation"
-#endif
-
-static esp_err_t i2c_master_init(void)
-{
-    int i2c_master_port = I2C_MASTER_NUM;
-
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = CONFIG_I2C_MASTER_SDA,
-        .scl_io_num = CONFIG_I2C_MASTER_SCL,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,
-    };
-
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-}
-
 static uint8_t hw_init_done = 0;
 
 static void io_fast_task(void * data){
@@ -86,8 +51,6 @@ void badge23_main(void)
 {
     locks_init();
     ESP_LOGI(TAG, "Starting on %s...", flow3r_bsp_hw_name);
-    ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C initialized successfully");
 
     audio_init();
     leds_init();
