48594b2be5035a75fe779ed82f451e215e657999
Serge Bazanski <q3k@q3k.org>
2023-07-04 01:49:33 +0200

    usermodule/badge_link: new pretty API

---
+++ python_payload/mypystubs/badge_link.pyi
@@ -0,0 +1,146 @@
+"""
+Badgelink API.
+
+Badgelink allows for multiple flow3r badges to connect together over the TRRS
+jacks and exchange data. It also allows for flow3r badges to access the outside
+world over the same connector, even if that world is outside of the realm of
+usual audio frequencies. Think: eurorack control voltages, MIDI, ...
+
+Currently the API is very spartan, and only exposes Jack objects for the left
+and right TRRS jack. These Jack objects in return have standard micropython
+ESP32 Pin objects on which you can instantiate other micropython ESP32 objects,
+like UART, PWM, etc.
+
+We hope to have a high-level API that allows actually establishing connectivity
+between two badges very soon :).
+
+Sample usage:
+
+>>> import badge_link
+>>> badge_link.right
+Jack(right)
+>>> badge_link.right.active()
+False
+>>> badge_link.right.enable()
+True
+>>> badge_link.right.active()
+True
+>>> badge_link.right.tip
+Pin(4)
+>>> rt = badge_link.right.tip
+>>> rt.init(mode=machine.Pin.OUT)
+>>> rt.on()
+"""
+
+from typing_extensions import Protocol
+
+class Pin(Protocol):
+    """
+    TODO(q3k): properly define in machine.pyi
+    """
+    pass
+
+
+class JackPin(Protocol):
+    @property
+    def tip(self) -> Pin:
+        """
+        Return the GPIO Pin object for this jack pin.
+
+        This pin can be manipulated to perform I/O on the jack once it is in
+        badgelink mode.
+        """
+        ...
+
+    def enable(self) -> bool:
+        """
+        Enable badgelink on this jack, on both ring and tip connectors.
+
+        Enabling badgelink means that the given jack will not be used to pass
+        through audio (ie. line out / headphones out on the left jack, and line
+        in on the right jack). Instead, that jack will be connected to the
+        ESP32's pins, and normal ESP32 I/O operations can be performed on said
+        pins.
+
+        True is returned if badgelink was succesfully enabled, false otherwies.
+        Badgelink will fail to initialize on the left jack if nothing is
+        currently plugged in (as a safety measure).
+        """
+        ...
+
+    def disable(self) -> None:
+        """
+        Disable badgelink on this jack, returning it into audio mode.
+        """
+        ...
+
+    def active(self) -> bool:
+        """
+        Returns true if this pin is currently enabled for badgelink, false if
+        it's in standard audio mode.
+        """
+        ...
+
+class Jack(Protocol):
+    def enable(self) -> bool:
+        """
+        Enable badgelink on this jack, on both ring and tip connectors.
+
+        Enabling badgelink means that the given jack will not be used to pass
+        through audio (ie. line out / headphones out on the left jack, and line
+        in on the right jack). Instead, that jack will be connected to the
+        ESP32's pins, and normal ESP32 I/O operations can be performed on said
+        pins.
+
+        True is returned if badgelink was succesfully enabled, false otherwies.
+        Badgelink will fail to initialize on the left jack if nothing is
+        currently plugged in (as a safety measure).
+
+        This enables both pins (tip and ring) of this jack. They can also be
+        individually enabled using the .pin.enable/.pin.disable methods.
+        """
+        ...
+
+    def disable(self) -> None:
+        """
+        Disable badgelink on this jack, returning it into audio mode.
+
+        This disables both pins (tip and ring) of this jack. They can also be
+        individually disabled using the .pin.enable/.pin.disable methods.
+        """
+        ...
+
+    def active(self) -> bool:
+        """
+        Returns true if this jack is currently fully enabled for badgelink,
+        false if it's in standard audio mode.
+
+        If only one of the pins is individually enabled, this method returns
+        false.
+        """
+        ...
+
+    @property
+    def tip(self) -> JackPin:
+        """
+        Return the JackPin object for the tip of this jack.
+
+        This pin can be manipulated to perform I/O on the jack once it is in
+        badgelink mode. It can also be used to individdually enable/disable
+        badge link on this pin only.
+        """
+        ...
+
+    @property
+    def ring(self) -> JackPin:
+        """
+        Return the JackPin object for the ring of this jack.
+
+        This pin can be manipulated to perform I/O on the jack once it is in
+        badgelink mode. It can also be used to individdually enable/disable
+        badge link on this pin only.
+        """
+        ...
+
+left: Jack
+right: JackKhizqrU9ANxlu5sP
--- usermodule/mp_badge_link.c
+++ usermodule/mp_badge_link.c
@@ -11,37 +11,210 @@
 #include "py/builtin.h"
 #include "py/runtime.h"
 
-#include "badge23/captouch.h"
 #include "st3m_io.h"
+#include "flow3r_bsp.h"
 
-STATIC mp_obj_t mp_get_active(mp_obj_t pin_mask) {
-    return mp_obj_new_int(st3m_io_badge_link_get_active(mp_obj_get_int(pin_mask)));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_get_active_obj, mp_get_active);
+// Badgelink API.
+//
+// See mypystubs/badge_link.pyi for more information.
 
-STATIC mp_obj_t mp_enable(mp_obj_t pin_mask) {
-    return mp_obj_new_int(st3m_io_badge_link_enable(mp_obj_get_int(pin_mask)));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_enable_obj, mp_enable);
+typedef struct _badgelink_jack_t {
+    mp_obj_base_t base;
+    bool left;
 
-STATIC mp_obj_t mp_disable(mp_obj_t pin_mask) {
-    return mp_obj_new_int(st3m_io_badge_link_disable(mp_obj_get_int(pin_mask)));
+    // Lazy initialized on first access.
+    mp_obj_t pin_tip;
+    mp_obj_t pin_ring;
+} badgelink_jack_t;
+
+const mp_obj_type_t badgelink_jack_type;
+
+typedef struct _badgelink_jack_pin_t {
+    mp_obj_base_t base;
+
+    bool left;
+    bool tip;
+
+    mp_obj_t pin;
+} badgelink_jack_pin_t;
+
+const mp_obj_type_t badgelink_jack_pin_type;
+
+STATIC badgelink_jack_t left = {
+    .base = {&badgelink_jack_type},
+    .left = true,
+
+    .pin_tip = mp_const_none,
+    .pin_ring = mp_const_none,
+};
+STATIC badgelink_jack_t right = {
+    .base = {&badgelink_jack_type},
+    .left = false,
+
+    .pin_tip = mp_const_none,
+    .pin_ring = mp_const_none,
+};
+
+STATIC void badgelink_jack_pin_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
+    badgelink_jack_pin_t *self = MP_OBJ_TO_PTR(self_in);
+
+    if (self->left) {
+        if (self->tip)
+            mp_printf(print, "JackPin(left tip)");
+        else
+            mp_printf(print, "JackPin(left ring)");
+    } else {
+        if (self->tip)
+            mp_printf(print, "JackPin(right tip)");
+        else
+            mp_printf(print, "JackPin(right ring)");
+    }
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mp_disable_obj, mp_disable);
+
+STATIC void badgelink_jack_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
+    badgelink_jack_t *self = MP_OBJ_TO_PTR(self_in);
+
+    if (self->left)
+        mp_printf(print, "Jack(left)");
+    else
+        mp_printf(print, "Jack(right)");
+}
+
+// From machine_pin.c. Used to make a machine.Pin below.
+mp_obj_t mp_pin_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args);
+
+STATIC mp_obj_t badgelink_jack_pin_make_new(bool left, bool tip) {
+    badgelink_jack_pin_t *self = m_new_obj(badgelink_jack_pin_t);
+    self->base.type = &badgelink_jack_pin_type;
+    self->left = left;
+    self->tip = tip;
+
+    uint32_t unum = left
+        ? (tip
+            ? flow3r_bsp_spio_programmable_pins.badgelink_left_tip
+            : flow3r_bsp_spio_programmable_pins.badgelink_left_ring)
+        : (tip
+            ? flow3r_bsp_spio_programmable_pins.badgelink_right_tip
+            : flow3r_bsp_spio_programmable_pins.badgelink_right_ring);
+    mp_obj_t num = mp_obj_new_int_from_uint(unum);
+    self->pin = mp_pin_make_new(NULL, 1, 0, &num);
+
+    return MP_OBJ_FROM_PTR(self);
+}
+
+STATIC void badgelink_jack_pin_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest) {
+    badgelink_jack_pin_t *self = MP_OBJ_TO_PTR(self_in);
+    if (dest[0] != MP_OBJ_NULL) {
+        return;
+    }
+    switch (attr) {
+    case MP_QSTR_pin: dest[0] = self->pin; break;
+    default:
+        dest[1] = MP_OBJ_SENTINEL;
+    }
+}
+
+STATIC void badgelink_jack_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest) {
+    badgelink_jack_t *self = MP_OBJ_TO_PTR(self_in);
+    if (dest[0] != MP_OBJ_NULL) {
+        return;
+    }
+    if (self->pin_tip == mp_const_none) {
+        self->pin_tip = badgelink_jack_pin_make_new(self->left, true);
+    }
+    if (self->pin_ring == mp_const_none) {
+        self->pin_ring = badgelink_jack_pin_make_new(self->left, false);
+    }
+    switch (attr) {
+    case MP_QSTR_tip: dest[0] = self->pin_tip; break;
+    case MP_QSTR_ring: dest[0] = self->pin_ring; break;
+    default:
+        dest[1] = MP_OBJ_SENTINEL;
+    }
+}
+
+STATIC uint8_t pin_mask_for_jack(mp_obj_t self_in) {
+    const mp_obj_type_t *ty = mp_obj_get_type(self_in);
+
+    uint8_t pin_mask = 0;
+	bool left = false;
+    bool tip = false;
+    bool ring = false;
+    if (ty == &badgelink_jack_pin_type) {
+        badgelink_jack_pin_t *self_jack_pin = MP_OBJ_TO_PTR(self_in);
+        left = self_jack_pin->left;
+        tip = self_jack_pin->tip;
+        ring = !tip;
+    } else {
+        badgelink_jack_t *self_jack = MP_OBJ_TO_PTR(self_in);
+        left = self_jack->left;
+        tip = true;
+        ring = true;
+    }
+
+    if (left && tip) pin_mask |= BADGE_LINK_PIN_MASK_LINE_OUT_TIP;
+    if (left && ring) pin_mask |= BADGE_LINK_PIN_MASK_LINE_OUT_RING;
+    if (!left && tip) pin_mask |= BADGE_LINK_PIN_MASK_LINE_IN_TIP;
+    if (!left && ring) pin_mask |= BADGE_LINK_PIN_MASK_LINE_IN_RING;
+    return pin_mask;
+}
+
+// Shared between Jack and JackPin.
+STATIC mp_obj_t badgelink_jack_active(mp_obj_t self_in) {
+    uint8_t pin_mask = pin_mask_for_jack(self_in);
+    uint8_t val = st3m_io_badge_link_get_active(pin_mask);
+    return mp_obj_new_bool(val == pin_mask);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(badgelink_jack_active_obj, badgelink_jack_active);
+
+// Shared between Jack and JackPin.
+STATIC mp_obj_t badgelink_jack_enable(mp_obj_t self_in) {
+    uint8_t pin_mask = pin_mask_for_jack(self_in);
+    uint8_t val = st3m_io_badge_link_enable(pin_mask);
+    return mp_obj_new_bool(val == pin_mask);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(badgelink_jack_enable_obj, badgelink_jack_enable);
+
+// Shared between Jack and JackPin.
+STATIC mp_obj_t badgelink_jack_disable(mp_obj_t self_in) {
+    uint8_t pin_mask = pin_mask_for_jack(self_in);
+    st3m_io_badge_link_disable(pin_mask);
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(badgelink_jack_disable_obj, badgelink_jack_disable);
+
+// Shared between Jack and JackPin.
+STATIC const mp_rom_map_elem_t badgelink_jack_locals_dict_table[] = {
+    // instance methods
+    { MP_ROM_QSTR(MP_QSTR_enable), MP_ROM_PTR(&badgelink_jack_enable_obj) },
+    { MP_ROM_QSTR(MP_QSTR_disable), MP_ROM_PTR(&badgelink_jack_disable_obj) },
+    { MP_ROM_QSTR(MP_QSTR_active), MP_ROM_PTR(&badgelink_jack_active_obj) },
+};
+
+STATIC MP_DEFINE_CONST_DICT(badgelink_jack_locals_dict, badgelink_jack_locals_dict_table);
+
+MP_DEFINE_CONST_OBJ_TYPE(
+    badgelink_jack_pin_type,
+    MP_QSTR_JackPin,
+    MP_TYPE_FLAG_NONE,
+    print, badgelink_jack_pin_print,
+    attr, badgelink_jack_pin_attr,
+    locals_dict, &badgelink_jack_locals_dict
+);
+
+MP_DEFINE_CONST_OBJ_TYPE(
+    badgelink_jack_type,
+    MP_QSTR_Jack,
+    MP_TYPE_FLAG_NONE,
+    print, badgelink_jack_print,
+    attr, badgelink_jack_attr,
+    locals_dict, &badgelink_jack_locals_dict
+);
 
 STATIC const mp_rom_map_elem_t mp_module_badge_link_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_badge_link) },
-    { MP_ROM_QSTR(MP_QSTR_get_active), MP_ROM_PTR(&mp_get_active_obj) },
-    { MP_ROM_QSTR(MP_QSTR_enable), MP_ROM_PTR(&mp_enable_obj) },
-    { MP_ROM_QSTR(MP_QSTR_disable), MP_ROM_PTR(&mp_disable_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_PIN_MASK_LINE_IN_TIP), MP_ROM_INT(BADGE_LINK_PIN_MASK_LINE_IN_TIP) },
-    { MP_ROM_QSTR(MP_QSTR_PIN_MASK_LINE_IN_RING), MP_ROM_INT(BADGE_LINK_PIN_MASK_LINE_IN_RING) },
-    { MP_ROM_QSTR(MP_QSTR_PIN_MASK_LINE_OUT_TIP), MP_ROM_INT(BADGE_LINK_PIN_MASK_LINE_OUT_TIP) },
-    { MP_ROM_QSTR(MP_QSTR_PIN_MASK_LINE_OUT_RING), MP_ROM_INT(BADGE_LINK_PIN_MASK_LINE_OUT_RING) },
-    { MP_ROM_QSTR(MP_QSTR_PIN_MASK_LINE_IN), MP_ROM_INT(BADGE_LINK_PIN_MASK_LINE_IN) },
-    { MP_ROM_QSTR(MP_QSTR_PIN_MASK_LINE_OUT), MP_ROM_INT(BADGE_LINK_PIN_MASK_LINE_OUT) },
-    { MP_ROM_QSTR(MP_QSTR_PIN_MASK_ALL), MP_ROM_INT(BADGE_LINK_PIN_MASK_ALL) },
+    { MP_ROM_QSTR(MP_QSTR_left), MP_ROM_PTR(&left) },
+    { MP_ROM_QSTR(MP_QSTR_right), MP_ROM_PTR(&right) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(mp_module_badge_link_globals, mp_module_badge_link_globals_table);
