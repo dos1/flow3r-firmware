8479e0f5edf5c8eb807ffed67e26d8fc8d1f7a81
Angus Gratton <angus@redyak.com.au>
2023-08-02 16:51:07 +1000

    esp32: Enable automatic Python heap growth.
    Via MICROPY_GC_SPLIT_HEAP_AUTO feature flag added in previous commit.
    
    Tested on ESP32 GENERIC_SPIRAM and GENERIC_S3 configurations, with some
    worst-case allocation patterns and the standard test suite.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- components/micropython/vendor/ports/esp32/mpconfigport.h
+++ components/micropython/vendor/ports/esp32/mpconfigport.h
@@ -68,6 +68,9 @@
 #define MICROPY_PY_THREAD_GIL               (1)
 #define MICROPY_PY_THREAD_GIL_VM_DIVISOR    (32)
 
+#define MICROPY_GC_SPLIT_HEAP               (1)
+#define MICROPY_GC_SPLIT_HEAP_AUTO          (1)
+
 // extended modules
 #ifndef MICROPY_PY_BLUETOOTH
 #define MICROPY_PY_BLUETOOTH                (1)
