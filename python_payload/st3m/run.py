7e52c1192a59e22d665d52b0b460b821947adf4a
Serge Bazanski <q3k@q3k.org>
2023-08-13 14:28:36 +0200

    st3m: remove one of two Reboot menu positions

--- python_payload/st3m/run.py
+++ python_payload/st3m/run.py
@@ -135,7 +135,6 @@ def run_main() -> None:
             MenuItemAction("Disk Mode (SD)", machine.disk_mode_sd),
             MenuItemLaunchPersistentView("About", About),
             MenuItemAction("Yeet Local Changes", _yeet_local_changes),
-            MenuItemAction("Reboot", lambda: machine.reset()),
             MenuItemAction("Reboot", machine.reset),
         ],
     )
