cd819d50dc6fc588f2dad3a436f2541c986e4ed4
Rahix <rahix@rahix.de>
2023-08-22 22:40:39 +0200

    mypy: Stubs more stubs for `network` and `urequests`

--- python_payload/mypystubs/network.pyi
+++ python_payload/mypystubs/network.pyi
@@ -1,4 +1,5 @@
-from st3m.goose import Optional
+from st3m.goose import Optional, Tuple, List, Any
+from typing import overload
 
 STA_IF: int
 
@@ -8,3 +9,33 @@ class WLAN:
     def connect(self, ssid: bytes, key: Optional[bytes] = None) -> None: ...
     def isconnected(self) -> bool: ...
     def status(self, s: str) -> float: ...
+    def scan(self) -> List[Tuple[bytes, bytes, int, int, int, bool]]: ...
+    @overload
+    def ifconfig(self) -> Tuple[str, str, str, str]: ...
+    @overload
+    def ifconfig(self, cfg: Optional[Tuple[str, str, str, str]]) -> None: ...
+    @overload
+    def config(self, setting: str) -> Any: ...
+    @overload
+    def config(
+        self,
+        *,
+        mac: bytes = b"",
+        ssid: str = "",
+        channel: int = 0,
+        hidden: bool = False,
+        security: int = 0,
+        key: str = "",
+        reconnects: int = 0,
+        txpower: float = 0.0,
+        pm: int = 0,
+    ) -> None: ...
+
+@overload
+def country() -> str: ...
+@overload
+def country(country_code: str) -> None: ...
+@overload
+def hostname() -> str: ...
+@overload
+def hostname(hostname: str) -> None: ...
--- python_payload/mypystubs/urequests.pyi
+++ python_payload/mypystubs/urequests.pyi
@@ -1,19 +1,54 @@
-from typing import Any
+from typing import Any, Dict, Optional
 
 class Response:
     text: str
     content: bytes
     status_code: int
+    headers: Dict[str, str]
 
     def close(self) -> None: ...
     def json(self) -> Any: ...
 
 def request(
-    method: str, url: str, data: Any, json: Any, headers: dict[str, str]
+    method: str,
+    url: str,
+    data: Any = None,
+    json: Any = None,
+    headers: Optional[Dict[str, str]] = None,
 ) -> Response: ...
-def head(url: str, **kw: Any) -> Response: ...
-def get(url: str, **kw: Any) -> Response: ...
-def post(url: str, **kw: Any) -> Response: ...
-def put(url: str, **kw: Any) -> Response: ...
-def patch(url: str, **kw: Any) -> Response: ...
-def delete(url: str, **kw: Any) -> Response: ...
+def head(
+    url: str,
+    data: Any = None,
+    json: Any = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> Response: ...
+def get(
+    url: str,
+    data: Any = None,
+    json: Any = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> Response: ...
+def post(
+    url: str,
+    data: Any = None,
+    json: Any = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> Response: ...
+def put(
+    url: str,
+    data: Any = None,
+    json: Any = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> Response: ...
+def patch(
+    url: str,
+    data: Any = None,
+    json: Any = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> Response: ...
+def delete(
+    url: str,
+    data: Any = None,
+    json: Any = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> Response: ...
