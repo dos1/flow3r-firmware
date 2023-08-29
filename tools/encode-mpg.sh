c596ce0c3c1240eabad9ed9c8b3a4b07683b5aaf
Øyvind Kolås <pippin@gimp.org>
2023-08-29 07:48:54 +0200

    tools: add mpg1 ffmpeg transcoding script

---
+++ tools/encode-mpg.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e -x
+if (( $# != 2 )); then
+    >&2 echo "Usage: encode-mpg <video-file> <output-file>"
+    exit
+fi
+
+ffmpeg -i $1 -vf scale=128:96 -c:v mpeg1video -b:v 96k -ac 1 -c:a mp2 -format mpeg -b:a 64k  -ar 48000  -r 24 $2 
+
+
