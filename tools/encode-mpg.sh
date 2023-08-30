177c2252d7348303ee0ec64bd94d03ac94ab1a3f
Sebastian Krzyszkowiak <dos@dosowisko.net>
2023-08-31 00:11:56 +0200

    encode-mpg: Correctly handle arguments with spaces

--- tools/encode-mpg.sh
+++ tools/encode-mpg.sh
@@ -5,6 +5,6 @@ if (( $# != 2 )); then
     exit
 fi
 
-ffmpeg -i $1 -vf scale=128:96 -c:v mpeg1video -b:v 96k -ac 1 -c:a mp2 -format mpeg -b:a 64k  -ar 48000  -r 24 $2 
+ffmpeg -i "$1" -vf scale=128:96 -c:v mpeg1video -b:v 96k -ac 1 -c:a mp2 -format mpeg -b:a 64k  -ar 48000  -r 24 "$2"
 
 
