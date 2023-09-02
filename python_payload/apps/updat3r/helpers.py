e6f8fcffa05078a56446c9198c645aa446daf5cc
ave <ave@ave.zone>
2023-09-02 14:03:30 +0200

    add stub for checking sd is stock

--- python_payload/apps/updat3r/helpers.py
+++ python_payload/apps/updat3r/helpers.py
@@ -8,3 +8,8 @@ def sd_card_plugged() -> bool:
     except OSError:
         # OSError: [Errno 19] ENODEV
         return False
+
+
+def is_sd_card_stock() -> bool:
+    # TODO: check if ~512mb
+    return True
