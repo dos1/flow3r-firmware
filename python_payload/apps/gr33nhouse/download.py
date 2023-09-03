68e3b8a654178610a1912d0735e457dcfe4e9daa
ave <ave@ave.zone>
2023-09-03 17:44:55 +0200

    gr33nhouse: display download progress

--- python_payload/apps/gr33nhouse/download.py
+++ python_payload/apps/gr33nhouse/download.py
@@ -1,5 +1,5 @@
 from st3m.input import InputController, InputState
-from st3m.goose import Optional
+from st3m.goose import Optional, List
 from st3m.ui import colours
 from urllib.urequest import urlopen
 import gzip
@@ -12,8 +12,9 @@ from ctx import Context
 
 
 class DownloadView(BaseView):
-    response: Optional[bytes]
+    response: Optional[bytes] = b""
     error_message: str = ""
+    _download_instance: Optional["download_file"]
 
     """
     View state
@@ -34,6 +35,8 @@ class DownloadView(BaseView):
         self._state = 1
         self._try = 1
         self._url = url
+        self.response = b""
+        self._download_instance = None
 
         self.input = InputController()
 
@@ -52,6 +55,8 @@ class DownloadView(BaseView):
         if self._state == 1 or self._state == 2:
             # Fetching
             text_to_draw = "Downloading..."
+            if self._download_instance:
+                text_to_draw += f"\n{len(self.response)}b"
             self._state = 2
         elif self._state == 3 or self._state == 4:
             # Extracting
@@ -67,41 +72,57 @@ class DownloadView(BaseView):
             ctx.move_to(0, -30)
             ctx.text("Oops...")
             text_to_draw = self.error_message
+            ctx.font_size = 12
 
         y_offset = 0
         for line in text_to_draw.split("\n"):
             ctx.move_to(0, y_offset)
             ctx.text(line)
-            y_offset += 30
+            y_offset += int(ctx.font_size * 1.25)
 
         ctx.restore()
 
-    def download_file(self, url: str, block_size=40960) -> Optional[bool]:
+    def download_file(self, url: str, block_size=40960) -> List[bytes]:
         gc.collect()
         req = urlopen(url)
-        self.response = b""
+        yield
 
         try:
             while True:
                 new_data = req.read(block_size)
-                self.response += new_data
+                yield new_data
                 if len(new_data) < block_size:
                     break
         finally:
             req.close()
-        return True
 
     def think(self, ins: InputState, delta_ms: int) -> None:
         # super().think(ins, delta_ms)  # Let BaseView do its thing
         self.input.think(ins, delta_ms)
 
+        if self.input.buttons.app.middle.pressed:
+            if self.vm is None:
+                raise RuntimeError("vm is None")
+            self.vm.pop()
+
         if self._state == 2:
             fail_reason = ""
             try:
-                print("Getting it")
-                download_result = self.download_file(self._url)
+                if not self._download_instance:
+                    print("Getting it")
+                    self.response = b""
+                    self._download_instance = self.download_file(self._url)
+                    return
+                else:
+                    try:
+                        new_data = next(self._download_instance)
+                        if new_data:
+                            self.response += new_data
+                        return
+                    except StopIteration:
+                        self._download_instance = None
 
-                if download_result and self.response is not None:
+                if self.response is not None:
                     print("Got something")
                     self._state = 3
                     return
@@ -160,8 +181,3 @@ class DownloadView(BaseView):
                     with open(filename, "wb") as of:
                         of.write(f.read())
             self._state = 5
-
-        if self.input.buttons.app.middle.pressed:
-            if self.vm is None:
-                raise RuntimeError("vm is None")
-            self.vm.pop()
