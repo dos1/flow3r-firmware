a6bde3c7c7be514bc9bafbf35f1b748540667256
Øyvind Kolås <pippin@gimp.org>
2023-10-31 20:26:38 +0100

    ctx: tune config
    Increase maximum edge list to 1024 elements, double the maximum number of
    segments per-scanline.

--- components/ctx/ctx_config.h
+++ components/ctx/ctx_config.h
@@ -34,7 +34,6 @@
 #define CTX_COMPOSITING_GROUPS             0
 #define CTX_ALWAYS_USE_NEAREST_FOR_SCALE1  1
 #define CTX_EVENTS                         0
-#define CTX_FORCE_INLINES                  0
 #define CTX_THREADS                        0
 #define CTX_TILED                          0
 #define CTX_BAREMETAL                      1
@@ -44,12 +43,12 @@
 #define CTX_MAX_JOURNAL_SIZE               (1024*512)
 // is also max and limits complexity
 // of paths that can be filled
-#define CTX_MIN_EDGE_LIST_SIZE             512
+#define CTX_MIN_EDGE_LIST_SIZE             1024
 
 #define CTX_MAX_DASHES                     32
 #define CTX_MAX_GRADIENT_STOPS             10
 #define CTX_MAX_STATES                     10
-#define CTX_MAX_EDGES                      127
+#define CTX_MAX_EDGES                      255
 #define CTX_MAX_PENDING                    64
 
 #define CTX_GRADIENT_CACHE_ELEMENTS        128
@@ -63,11 +62,16 @@
 #define CTX_MAX_KEYBINDINGS                16
 #define CTX_MAX_CBS                        8
 #define CTX_MAX_LISTEN_FDS                 1
+#define CTX_HASH_COLS                      5
+#define CTX_HASH_ROWS                      5
+#define CTX_STROKE_1PX                     1
 
 
 #if defined(CONFIG_FLOW3R_CTX_FLAVOUR_FULL)
 #define CTX_COMPOSITE_O3                1
+#define CTX_RASTERIZER_O2               0
 #define CTX_GSTATE_PROTECT              1
+#define CTX_FORCE_INLINES               1
 #define CTX_FRAGMENT_SPECIALIZE         1
 #define CTX_ENABLE_YUV420               1
 #define CTX_ENABLE_RGBA8                1
